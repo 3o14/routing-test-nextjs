16cd6e27ecfc8645a55f4e4ceacdd1c1cdc25a97
3o14 <dldnjswn4265@gmail.com>
2023-06-18 13:57:27 +0900

    :white_check_mark: Navigating with useRouter push
    useRouter.push("url")로 내비게이팅 하기

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -3,13 +3,17 @@ import { useRouter } from "next/router";
 
 function PortfolioPage() {
   const projects = [
-    { id: "placeQR", name: "Place.QR : QR코드 방명록 애플리케이션" },
+    { id: "placeqr", name: "Place.QR : QR코드 방명록 애플리케이션" },
     { id: "wooyeons", name: "우연한 발견: 위치기반 지도 익명 SNS" },
     { id: "carmeet", name: "카밋(carmmet): 랜덤 화상 통화 서비스" },
   ];
 
   const router = useRouter();
   console.log(router);
+
+  function loadProjectHandler() {
+    router.push("portfolio/placeqr");
+  }
 
   return (
     <div>
@@ -24,6 +28,8 @@ function PortfolioPage() {
           </li>
         ))}
       </ul>
+
+      <button onClick={loadProjectHandler}>프로젝트 보기</button>
     </div>
   );
 }
