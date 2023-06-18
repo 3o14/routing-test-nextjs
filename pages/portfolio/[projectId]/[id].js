d84bfe18c486c11a6112ad932bbfe373846eeb58
3o14 <dldnjswn4265@gmail.com>
2023-06-18 13:04:38 +0900

    :white_check_mark: Dynamic Route
    동적 라우팅& 경로

---
+++ pages/portfolio/[projectId]/[id].js
@@ -0,0 +1,9 @@
+function DynamicPage() {
+  return (
+    <div>
+      <h1>This is Dynamic Page.</h1>
+    </div>
+  );
+}
+
+export default DynamicPage;
---
+++ pages/portfolio/[projectId]/index.js
@@ -0,0 +1,10 @@
+function ProjectPage() {
+    return (
+      <div>
+        <h1>This is Project Page.</h1>
+      </div>
+    );
+  }
+  
+  export default ProjectPage;
+  
